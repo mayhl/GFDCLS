69a714525ae768b47ef28d97c751ce9495f5ffe0
mayhl <michaelangelo.yh.lam@gmail.com>
2018-08-09 11:25:41 -0700

    Added subroutines file for Memory namespace. See comments in files.

---
+++ include/memory/subroutines.h
@@ -0,0 +1,64 @@
+// ----------------------------------------------------------------------------------
+// Copyright 2016-2018 Michael-Angelo Yick-Hang Lam
+//
+// The development of this software was supported by the National Science Foundation
+// (NSF) Grant Number DMS-1211713.
+//
+// This file is part of GADIT.
+//
+// GADIT is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 as published by
+// the Free Software Foundation.
+//
+// GADIT is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GADIT.  If not, see <http://www.gnu.org/licenses/>.
+// ----------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------- //
+//                                                                      //
+// Memory Subroutines v1.0                                              //
+//																		//
+// Purpose: Various miscellaneous subroutines.                          //
+//                                                                      //
+// Notes: Due to the class template Memory_Unit, some functions were    //
+//        not added as static members to avoid repetitive definitions   //
+//        for each instantiation of the class template.                 //
+//                                                                      //
+// -------------------------------------------------------------------- //
+
+#ifndef MEMORY_SUBROUTINES_H
+#define MEMORY_SUBROUTINES_H
+
+#include <string>
+
+#include "include/memory/base_unit.h"
+#include "include/memory/unit.h"
+
+namespace Memory
+{
+	bool isMemoryUnitsSame(Memory::Base_Unit *base_primary, Memory::Base_Unit *base_secondary, std::string &message);
+	template<typename> bool isMemoryUnitsSameSubroutine(Memory::Base_Unit *base_primary, Memory::Base_Unit *base_secondary);
+
+};
+
+// Note: Until more function templates are added, a seperated .tpp source file
+//       was not added to avoid extraneous files.
+template<typename D> bool isMemoryUnitsSameSubroutine(Memory::Base_Unit *base_primary, Memory::Base_Unit *base_secondary )
+{
+	Memory::Unit<D> *derived_primary;
+	Memory::Unit<D> *derived_secondary;
+
+	// Note: Dynamic cast returns null pointer if particular base class  
+	//       Base_Unit objects are not of derived Unit<D> type.
+	derived_primary = dynamic_cast<Memory::Unit<D>*>(base_primary);
+	derived_secondary = dynamic_cast<Memory::Unit<D>*>(base_secondary);
+
+	return (derived_primary != 0) && (derived_secondary != 0);
+}
+
+#endif
---
+++ src/memory/subroutines.cpp
@@ -0,0 +1,16 @@
+#include "include/memory/subroutines.h"
+
+namespace Memory
+{
+	bool isMemoryUnitsSame(Memory::Base_Unit *base_primary, Memory::Base_Unit *base_secondary, std::string &message)
+	{
+		// Note: If other versions of class template Unit are instantiated, subroutine
+		//       needs to be modified. 
+		if (isMemoryUnitsSameSubroutine<double>(base_primary, base_secondary)) return true;
+		if (isMemoryUnitsSameSubroutine<char>(base_primary, base_secondary)) return true;
+		if (isMemoryUnitsSameSubroutine<float>(base_primary, base_secondary)) return true;
+
+		message = "ERROR: Memory Units are not of the same type.\n";
+		return false;
+	}
+};
