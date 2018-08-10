d59790c027485d4ae1ac8aa54f1dd4879b98f08f
mayhl <michaelangelo.yh.lam@gmail.com>
2018-08-10 15:33:28 -0700

    Added cudaErrorToString command to String Processing.

--- src/string_processing.cpp
+++ src/string_processing.cpp
@@ -1,3 +1,24 @@
+// -------------------------------------------------------------------- //
+// Copyright 2016-2018 Michael-Angelo Yick-Hang Lam                     //
+//                                                                      //
+// The development of this software was supported by the National       //
+// Science Foundation (NSF) Grant Number DMS-1211713.                   //
+//                                                                      //
+// This file is part of GADIT.                                          //
+//                                                                      //
+// GADIT is free software: you can redistribute it and/or modify it     //
+// under the terms of the GNU General Public License version 3 as       //
+// published by the Free Software Foundation.                           //
+//                                                                      //
+// GADIT is distributed in the hope that it will be useful, but WITHOUT //
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   //
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public     //
+// License for more details.                                            //
+//                                                                      //
+// You should have received a copy of the GNU General Public License    //
+// along with GADIT.  If not, see <http://www.gnu.org/licenses/>.       //
+// ---------------------------------------------------------------------//
+
 #include "include/string_processing.h"
 
 namespace StringProcessing
@@ -22,5 +43,11 @@ namespace StringProcessing
 			str.replace(start_pos, newline.length(), newline_indent );
 			start_pos += newline_indent.length();
 		}
+	}
+
+	void cudaErrorToString(cudaError error, std::string &message)
+	{
+		message =  "Type   : " + std::string(cudaGetErrorName(error)) + "\n";
+		message += "Message: " + std::string(cudaGetErrorString(error)) + "\n";
 	}
 };
