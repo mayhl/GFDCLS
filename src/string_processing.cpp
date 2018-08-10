e58a817c5336175b9dc35d66aad2d97c466e9698
mayhl <michaelangelo.yh.lam@gmail.com>
2018-08-10 10:21:23 -0700

    Cleaned up String Processing code.

--- src/string_processing.cpp
+++ src/string_processing.cpp
@@ -2,28 +2,25 @@
 
 namespace StringProcessing
 {
-
-
 	void indentLines(std::string &str)
 	{
-		size_t start_pos = 0;
-		const std::string newline = "\n";
+		// Changeable indent length to control the width of output text
 		const std::string indent = "  ";
+		const std::string newline = "\n";
 		const std::string newline_indent = newline + indent;
 
-		// indent first line
+		// Indent first line
 		str.insert(0, indent);
-
-		while ((start_pos = str.find(newline, start_pos)) != std::string::npos) {
+		
+		size_t start_pos = 0;
+		// Indent after new line characters
+		while ((start_pos = str.find(newline, start_pos)) != std::string::npos) 
+		{
 			// Do not indent after last new line character.
-			if (start_pos == str.length() - 1)
-				break;
-			// Indent all other lines after new line chracter.
+			if ( start_pos == str.length() - 1 ) break;
+
 			str.replace(start_pos, newline.length(), newline_indent );
 			start_pos += newline_indent.length();
-
 		}
-
 	}
-
 };
