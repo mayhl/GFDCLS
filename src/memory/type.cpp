df240a67e9bd92073d663081a214f7e57ddc0e2e
mayhl <michaelangelo.yh.lam@gmail.com>
2018-08-13 08:15:04 -0700

    Changed license from GNU GPL v 3.0 to BSD 2 - Clause.

--- src/memory/type.cpp
+++ src/memory/type.cpp
@@ -1,23 +1,37 @@
-// -------------------------------------------------------------------- //
-// Copyright 2016-2018 Michael-Angelo Yick-Hang Lam                     //
-//                                                                      //
-// The development of this software was supported by the National       //
-// Science Foundation (NSF) Grant Number DMS-1211713.                   //
-//                                                                      //
-// This file is part of GADIT.                                          //
-//                                                                      //
-// GADIT is free software: you can redistribute it and/or modify it     //
-// under the terms of the GNU General Public License version 3 as       //
-// published by the Free Software Foundation.                           //
-//                                                                      //
-// GADIT is distributed in the hope that it will be useful, but WITHOUT //
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY   //
-// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public     //
-// License for more details.                                            //
-//                                                                      //
-// You should have received a copy of the GNU General Public License    //
-// along with GADIT.  If not, see <http://www.gnu.org/licenses/>.       //
-// ---------------------------------------------------------------------//
+//////////////////////////////////////////////////////////////////////////////////////
+//                                                                                  //
+//  BSD 2 - Clause License                                                          //
+//                                                                                  //
+//  Copyright(c) 2018, Michael-Angelo Yick-Hang Lam                                 //
+//  All rights reserved.                                                            //
+//                                                                                  //
+//  Redistribution and use in source and binary forms, with or without              //
+//  modification, are permitted provided that the following conditions are met :    //
+//                                                                                  //
+//  * Redistributions of source code must retain the above copyright notice, this   //
+//  list of conditions and the following disclaimer.                                //
+//                                                                                  //
+//  * Redistributions in binary form must reproduce the above copyright notice,     //
+//    this list of conditions and the following disclaimer in the documentation     //
+//    and / or other materials provided with the distribution.                      //
+//                                                                                  //
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"     //
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE       //
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  //
+//  DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE     //
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL      //
+//  DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR       //
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER      //
+//  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,   //
+//  OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    //
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.            //
+//                                                                                  //
+//  The development of this software was supported by the National                  //
+//  Science Foundation (NSF) Grant Number DMS-1211713.                              //
+//                                                                                  //
+//  This file is part of GPU Finite Difference Conservation Law Solver (GFDCLS).    //
+//                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////
 
 #include "include/memory/type.h"
 
